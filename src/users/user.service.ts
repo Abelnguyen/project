5c442c4b45cc600d3cad0b4b4c7ba1f78fba8b28
Anh Tuan Nguyen <nguyanht@yahoo.com>
2022-11-16 00:37:52 +0100

    init

---
+++ src/users/user.service.ts
@@ -0,0 +1,29 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { hashPassword } from '../utils/helpers';
+import { User } from '../utils/typeorm';
+import { CreateUserDetails, FindUserParams } from '../utils/types';
+import { IUserService } from './user';
+
+@Injectable()
+export class UserService implements IUserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(userDetails: CreateUserDetails) {
+    const existingUser = await this.userRepository.findOneBy({
+      email: userDetails.email,
+    });
+    if (existingUser)
+      throw new HttpException('User already exists', HttpStatus.CONFLICT);
+    const password = await hashPassword(userDetails.password);
+    const newUser = this.userRepository.create({ ...userDetails, password });
+    return this.userRepository.save(newUser);
+  }
+
+  async findUser(findUserParams: FindUserParams): Promise<User> {
+    return this.userRepository.findOneBy(findUserParams);
+  }
+}
